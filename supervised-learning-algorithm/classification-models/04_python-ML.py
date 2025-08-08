e811154bfd8c312a798c91028ae06223ee856b05
Gauri <gauripatil2999@gmail.com>
2025-08-08 19:42:41 +0530

    Logistic Regression

---
+++ supervised-learning-algorithm/classification-models/04_python-ML.py
@@ -0,0 +1,28 @@
+" Supervised Learning: Logistic Regression "
+
+# import libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import numpy as np
+
+# load data (hours studied vs pass/fail)
+X = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
+y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1]) # 0 = fail, 1 = pass
+
+# splitting data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42)
+
+# train the model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# model predictions
+y_pred = model.predict(X_test)
+
+# evaluate model
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print("Accuracy: ", accuracy)
+print("Confusion Matrix:\n", conf_matrix)
