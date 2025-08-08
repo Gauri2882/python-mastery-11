b4d3ce6eaebe43f6bfce55407f7e8ad250aa0371
Gauri <gauripatil2999@gmail.com>
2025-08-08 20:12:55 +0530

    Naive Bayes

---
+++ supervised-learning-algorithm/classification-models/10_python-ML.py
@@ -0,0 +1,28 @@
+' Probabilistic Classifier : Naive Bayes '
+
+# import libraries
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import numpy as np
+
+# load data ( hours studied and prior grades vs. pass/fail)
+X = np.array([[1, 50], [2, 60], [3, 55], [4, 65], [5, 70], [6, 75], [7, 80], [8, 85], [9, 89], [10, 92]])
+y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1]) # 0 = fail, 1 = pass
+
+# splitting data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42)
+
+# train the model 
+model = GaussianNB() 
+model.fit(X_train, y_train)
+
+# make predictions
+y_pred = model.predict(X_test)
+
+# evaluate model
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Confusion Matrix:\n", conf_matrix)
