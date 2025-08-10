35f378a0a3ce32340f1bbd7c4cd8c0a54b9ba0c6
Gauri <gauripatil2999@gmail.com>
2025-08-10 16:24:39 +0530

    One-Class SVM

---
+++ anomaly-detection-algorithm/22_python-ML.py
@@ -0,0 +1,22 @@
+' Anomaly Detection: One-Class SVM '
+
+ # import libraries
+from sklearn.svm import OneClassSVM
+import numpy as np
+
+# sample data (normal data points clustered around 0)
+X = 0.3 * np.random.randn(100, 2)
+X_train = np.r_[X + 2, X -2] # create dataset with points around two cluster
+
+# new test data including some outliers
+X_test = np.r_[X + 2, X - 2, np.random.uniform(low = 6, high = 6, size = (20, 2))]
+
+# initialize and train the model
+model = OneClassSVM(gamma = 'auto', nu = 0.1)
+model.fit(X_train)
+
+# predict on test data (-1 indicates an anomaly, 1 indicates normal)
+predictions = model.predict(X_test)
+
+# display predictions
+print("Predictions:", predictions)
